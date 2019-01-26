1d6513609115d307149330027fe39763f72b188b
Pavlo <pawel.wowniuk@gmail.com>
2019-01-26 20:14:52 +0100

    Zadanie domowe 6.1

---
+++ src/Tablica.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Tablica {
+    public static void main(String[] args) {
+        String[] tablicaImion = { "Pawel", "Slawek", "Marcin"};
+        System.out.println (Arrays.toString (tablicaImion));
+    }
+}
